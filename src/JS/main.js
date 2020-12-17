801fa02050f08fe30e6fd4e574286f92f323344d
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2020-12-17 14:11:11 +0400

    responsive

--- src/JS/main.js
+++ src/JS/main.js
@@ -9,6 +9,7 @@ const toggler = document.querySelector(".switch");
 const mainContent = document.querySelector(".main");
 
 // details 
+const details = document.querySelector('.details')
 const year = document.querySelector(".details__year");
 const week = document.querySelector(".details__week");
 const day = document.querySelector(".details__day");
@@ -96,8 +97,7 @@ async function getQuotes() {
         <p class="quotes__text">${responseData.content}</p>
         <p class="quotes__author">${responseData.author}</p>
       </section>
-      <button class="quotes__btn" onclick="getQuotes()"><svg width="18" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M7.188 10.667a.208.208 0 01.147.355l-2.344 2.206a5.826 5.826 0 009.578-2.488l2.387.746A8.322 8.322 0 013.17 14.94l-2.149 2.022a.208.208 0 01-.355-.148v-6.148h6.52zm7.617-7.63L16.978.958a.208.208 0 01.355.146v6.23h-6.498a.208.208 0 01-.147-.356L13 4.765A5.825 5.825 0 003.43 7.26l-2.386-.746a8.32 8.32 0 0113.76-3.477z" fill: #000 fill-rule="nonzero" opacity=".5"/></svg>
-      </button>`
+      <button class="quotes__btn" onclick="getQuotes()"><img class="quotes__btn--text" src="./src/assets/icons/icon-refresh.svg" alt="refresh"></button>`
 
       if (responseData.author === null) {
         author.textContent = 'Unknown author'
@@ -148,11 +148,13 @@ function getTime(){
     mainContent.classList.add('day');
     dayNightIcon.classList.add('rotatable')
 		dayNightIcon.src = './src/assets/icons/icon-sun.svg';
-		dayNightIcon.setAttribute("alt", "sun icon");
+    dayNightIcon.setAttribute("alt", "sun icon");
+    details.style.backgroundColor = "rgba(255, 255, 255, 0.75)"
 	} else {
 		mainContent.classList.add('night');
 		dayNightIcon.src = './src/assets/icons/icon-moon.svg';
-		dayNightIcon.setAttribute("alt", "moon icon");
+    dayNightIcon.setAttribute("alt", "moon icon");
+    details.style.backgroundColor = " hsl(0, 0%, 0%)"
 	}
 
   let interval = (60 - (new Date()).getSeconds()) * 1000 + 5;
@@ -176,3 +178,4 @@ getTimezone();
 getLocation();
 getQuotes();
 getTime();
+
